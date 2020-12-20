5b5ccb4e419a6d6b7ba6df57f0ac5b1f15ff772a
Akshata Naik <37590188+akshata05@users.noreply.github.com>
2020-12-21 01:50:53 +0530

    Fourth

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -1,6 +1,6 @@
 package utils;
 
 public class Constant {
-public final static String CONFIG_PROPERTIES_DIRECTORY="Properties\\config.properties";
+public final static String CONFIG_PROPERTIES_DIRECTORY=System.getProperty("user.dir")+"Properties\\config.properties";
 public final static String CHROME_DRIVER_DIRECTORY=System.getProperty("user.dir")+"\\src\\test\\java\\Resource\\chromedriver.exe";
 }
