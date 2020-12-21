ce27b08548f2c373f06837c2fb4211d3c8c0ea23
Akshata Naik <37590188+akshata05@users.noreply.github.com>
2020-12-21 22:02:29 +0530

    Seventh

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -4,11 +4,11 @@ import java.net.URL;
 import java.net.http.HttpRequest;
 
 public class Constant {
-	public final static	URL path=Constant.class.getClassLoader().getResource("Properties\\config.properties");
-	public final static URL path2=Constant.class.getClassLoader().getResource("\\src\\test\\java\\Resource\\chromedriver.exe");
+	//public final static	URL path=Constant.class.getClassLoader().getResource("Properties\\config.properties");
+	//public final static URL path2=Constant.class.getClassLoader().getResource("\\src\\test\\java\\Resource\\chromedriver.exe");
 	 
-public final static String CONFIG_PROPERTIES_DIRECTORY=path.toString();
+public final static String CONFIG_PROPERTIES_DIRECTORY="C:\\Users\\Akshata Naik\\.jenkins\\workspace\\Automation\\Properties\\config.properties";
 //"Properties\\config.properties";
-public final static String CHROME_DRIVER_DIRECTORY=path2.toString();
+public final static String CHROME_DRIVER_DIRECTORY="C:\\Users\\Akshata Naik\\.jenkins\\workspace\\Automation\\src\\test\\java\\Resource\\chromedriver.exe";
 //"\\src\\test\\java\\Resource\\chromedriver.exe";
 }
